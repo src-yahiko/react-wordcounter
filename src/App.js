a8273a704dc9eb41a4217bcd3a48a785146ecd32
Omar Bittar <omar.bittar@bitrate-it.de>
2022-08-14 00:02:03 +0200

    tictactoe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./Navbar";
+import {
+  MinusCircleIcon,
+  PlusCircleIcon,
+  ExclamationCircleIcon,
+} from "@heroicons/react/solid";
+import { useState } from "react";
 
 function App() {
+  const newGame = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  const [board, setBoard] = useState(newGame);
+  const [rem, setRem] = useState(9);
+  const [turnOfPlus, setTurnOfPlus] = useState(true);
+  const [gameover, setGameover] = useState(false);
+  const [won, setWon] = useState(0);
+
+  const checkWin = (board, player) => {
+    [0, 1, 2].forEach((y) => {
+      if (
+        (board[0][y] === player) &
+        (board[1][y] === player) &
+        (board[2][y] === player)
+      ) {
+        setWon(player);
+        setGameover(true);
+      }
+    });
+
+    [0, 1, 2].forEach((x) => {
+      if (
+        (board[x][0] === player) &
+        (board[x][1] === player) &
+        (board[x][2] === player)
+      ) {
+        setWon(player);
+        setGameover(true);
+      }
+    });
+
+    if (
+      (board[0][0] === player) &
+      (board[1][1] === player) &
+      (board[2][2] === player)
+    ) {
+      setWon(player);
+      setGameover(true);
+    }
+
+    if (
+      (board[0][2] === player) &
+      (board[1][1] === player) &
+      (board[2][0] === player)
+    ) {
+      setWon(player);
+      setGameover(true);
+    }
+
+    if (rem <= 1) return setGameover(true);
+  };
+
+  const handleTurn = (row, col) => {
+    if (gameover) return;
+
+    setRem(rem - 1);
+
+    const player = turnOfPlus ? 1 : -1;
+    const copy = board;
+    copy[row][col] = player;
+    setBoard(copy);
+    checkWin(copy, player);
+    setTurnOfPlus(!turnOfPlus);
+  };
+
+  const resetGame = () => {
+    setBoard(newGame);
+    setTurnOfPlus(!setTurnOfPlus);
+    setWon(0);
+    setGameover(false);
+    setRem(9);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <div className="max-w-5xl mx-auto mt-10">
+        {!gameover && !turnOfPlus && (
+          <p className="p-2 w-fit mx-auto theme-bg-secondary rounded-xl shadow-xl theme-fc-primary">
+            Turn of
+            <MinusCircleIcon className="text-blue-500 h-8 w-8 mx-1 p-1 inline" />
+          </p>
+        )}
+        {!gameover && turnOfPlus && (
+          <p className="p-2 w-fit mx-auto theme-bg-secondary rounded-xl shadow-xl theme-fc-primary">
+            Turn of
+            <PlusCircleIcon className="text-red-500 h-8 w-8 mx-1 p-1 inline" />
+          </p>
+        )}
+        {gameover && won === 0 && (
+          <p className="p-2 w-fit mx-auto theme-bg-secondary rounded-xl shadow-xl theme-fc-primary">
+            Game Over: Tie!
+            <span
+              onClick={resetGame}
+              className="hover:cursor-pointer block underline upperspace text-center text-sm"
+            >
+              Play again
+            </span>
+          </p>
+        )}
+        {gameover && won === -1 && (
+          <p className="p-2 w-fit mx-auto theme-bg-secondary rounded-xl shadow-xl theme-fc-primary">
+            Game Over:
+            <MinusCircleIcon className="text-blue-500  h-8 w-8 mx-1 p-1 inline" />
+            won!
+            <span
+              onClick={resetGame}
+              className="hover:cursor-pointer block underline upperspace text-center text-sm"
+            >
+              Play again
+            </span>
+          </p>
+        )}
+        {gameover && won === 1 && (
+          <p className="p-2 w-fit mx-auto theme-bg-secondary rounded-xl shadow-xl theme-fc-primary">
+            Game Over:
+            <PlusCircleIcon className="text-red-500 h-8 w-8 mx-1 p-1 inline" />
+            won!
+            <span
+              onClick={resetGame}
+              className="hover:cursor-pointer block underline upperspace text-center text-sm"
+            >
+              Play again
+            </span>
+          </p>
+        )}
+
+        <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-screen text-center">
+          <div id="game">
+            {board.map((row, j) => (
+              <span key={j}>
+                {row.map((f, i) =>
+                  f === 0 ? (
+                    <ExclamationCircleIcon
+                      onClick={() => handleTurn(j, i)}
+                      key={j * 3 + i}
+                      className="active opacity-50 text-transparent"
+                    />
+                  ) : f === 1 ? (
+                    <PlusCircleIcon
+                      key={j * 3 + i}
+                      className="text-red-500 bg-red-300 dark:bg-red-900"
+                    />
+                  ) : (
+                    <MinusCircleIcon
+                      key={j * 3 + i}
+                      className="text-blue-500 bg-blue-300 dark:bg-blue-900"
+                    />
+                  )
+                )}
+                <br />
+              </span>
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
   );
 }
 
---
+++ src/Navbar.js
@@ -0,0 +1,90 @@
+import { SunIcon, MoonIcon } from "@heroicons/react/solid";
+import { useEffect, useState } from "react";
+
+function DarkModeToggler({ className }) {
+  const [darkmode, setDarkmode] = useState(
+    document.getElementsByTagName("html")[0].classList.contains("dark")
+  );
+
+  const toggleDarkmode = () => {
+    document.getElementsByTagName("html")[0].classList.toggle("dark");
+
+    if (document.getElementsByTagName("html")[0].classList.contains("dark")) {
+      localStorage.theme = "dark";
+    } else {
+      localStorage.theme = "light";
+    }
+
+    setTimeout(
+      () =>
+        setDarkmode(
+          document.getElementsByTagName("html")[0].classList.contains("dark")
+        ),
+      100
+    );
+  };
+
+  useEffect(() => {
+    if (
+      (localStorage.theme === "dark" ||
+        (!localStorage.theme &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)) &&
+      darkmode === false
+    ) {
+      document.getElementsByTagName("html")[0].classList.add("dark");
+      setDarkmode(true);
+      // toggleDarkmode();
+    }
+  }, [darkmode]);
+
+  return darkmode ? (
+    <SunIcon
+      onClick={toggleDarkmode}
+      className="h-10 md:h-12 p-2 md:p-3 scale-0 dark:scale-100"
+    />
+  ) : (
+    <MoonIcon
+      onClick={toggleDarkmode}
+      className="h-10 md:h-12 p-2 md:p-3 scale-100 dark:scale-0"
+    />
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="z-20 fixed top-0 w-full border-b shadow-lg">
+      <div className="max-w-7xl w-full h-full items-center flex mx-auto px-2 justify-between">
+        <div className="flex items-center">
+          <h1 className="md:border-r px-1 mr-1 md:mr-5 md:px-5 whitespace-nowrap">
+            TicTacToe
+          </h1>
+        </div>
+        <div className="flex items-center">
+          <DarkModeToggler></DarkModeToggler>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+function Sidebar() {
+  return (
+    <div className="z-10 fixed bottom-0 md:left-0 w-full md:w-auto md:h-screen mb-5 px-5 md:mb-0 md:px-0">
+      <aside className="md:border-0 md:h-full md:border-r shadow-2xl border theme-b-secondary">
+        <div className="w-full h-full items-center flex md:flex-col justify-center"></div>
+      </aside>
+    </div>
+  );
+}
+
+function Layout({ children }) {
+  return (
+    <div id="home" className="text-xl md:text-2xl pt-12">
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+      {children}
+    </div>
+  );
+}
+
+export default Layout;
